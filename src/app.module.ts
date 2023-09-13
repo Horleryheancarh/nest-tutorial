d71f88c1897cd2b988cb8c03d6ab28ddc2f2f2eb
Horleryheancarh <oladipoolayinka7@gmail.com>
2023-09-13 23:01:20 +0100

    connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { CustomersModule } from './api/customers/customers.module';
 import { UsersModule } from './api/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import entities from './database';
 
 @Module({
-  imports: [CustomersModule, UsersModule],
+  imports: [
+    CustomersModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: ':memory:',
+      entities,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
